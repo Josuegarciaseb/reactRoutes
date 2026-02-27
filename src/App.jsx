59ffff136e787b43f54dbd8319f375daafa1bfc0
Josuegarciaseb <edgarjosuegarciasebastian@gmail.com>
2026-02-27 15:25:57 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route} from 'react-router-dom'
+import './App.css'
+import Inicio from './vistas/Inicio'
+import Productos from './vistas/Productos'
+
+function App() {
+  return (
+    <Routes>  
+      <Route path="/" element={<Inicio />} />
+      <Route path="/productos" element={<Productos />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../componentes/Header/Header";
+
+function Footer() {
+    return (
+        <>
+            <div>
+                <Header />
+                <h2>Footer</h2>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
